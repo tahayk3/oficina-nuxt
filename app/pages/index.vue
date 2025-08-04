0099adf8ff84ce27f64a6ab4e5349958b0593a33
tahayk3 <tahayk3@gmail.com>
2025-08-04 11:28:37 -0600

    configurando index y el robots.txt para  bots de busqueda

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+useHead({
+  title: 'Inicio - Oficina de Abogados',
+  meta: [
+    { name: 'description', content: 'Bienvenido a la página principal de la oficina de la abogada Karen Mazariegos' }
+  ]
+})
+</script>
+
+<template>
+  <h1>Bienvenido a la página oficial de la oficina de la abogada Karen Mazariegos</h1>
+  <p>Esta es la página de inicio.</p>
+</template>
