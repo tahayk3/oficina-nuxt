f997b31b8d7d301c45ce6f95e6b4b8327350fa5c
tahayk3 <tahayk3@gmail.com>
2025-08-04 15:20:38 -0600

    agregando sitemap.xml.ts

---
+++ server/server/sitemap.xml.ts
@@ -0,0 +1,24 @@
+export default defineEventHandler((event) => {
+  setHeader(event, 'Content-Type', 'application/xml')
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://oficina-karen-mazariegos.netlify.app/</loc>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://oficina-karen-mazariegos.netlify.app/servicios</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>https://oficina-karen-mazariegos.netlify.app/contacto</loc>
+      <changefreq>monthly</changefreq>
+      <priority>0.5</priority>
+    </url>
+  </urlset>`
+
+  return sitemap
+})
