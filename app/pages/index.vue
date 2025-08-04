66923cb61b71e9891cfbfd16f2168150a9263645
tahayk3 <tahayk3@gmail.com>
2025-08-04 12:10:01 -0600

    agregando google-site-verification en useHead

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,6 +7,7 @@ useHead({
     { property: 'og:description', content: 'Oficina de abogados en Quetzaltenango, especializada en derecho familiar y civil.' },
     { property: 'og:type', content: 'website' },
     { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'google-site-verification', content: 'XmQ4R4VD1GRD6Orf7foyZquAnyaplQ64_3iUn76bvhk' }
   ]
 })
 </script>
