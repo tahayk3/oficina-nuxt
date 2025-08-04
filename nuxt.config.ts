156c3511a6b87109f2bcc5c8446115c28004aef8
tahayk3 <tahayk3@gmail.com>
2025-08-04 17:02:02 -0600

    configurando nuxt para despliegue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  ssr: true,
   nitro: {
     preset: 'netlify'
   },
-  ssr: true,  // asegúrate que esté habilitado para SSR (SEO)
   modules: [
     '@nuxt/image',
     '@nuxt/icon',
-    '@nuxt/ui'
-  ],
-    typescript: {
-    strict: true,
-    shim: false  // para evitar problemas con imports automáticos en TS
-  }
+    '@nuxt/ui',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots'
+  ]
 })
