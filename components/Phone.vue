aee54414fe3b1457bc3eb1943d6124bedfceaa1f
tahayk3 <tahayk3@gmail.com>
2025-08-14 13:37:40 -0600

    añadiendo enlaces de whatsapp

--- components/Phone.vue
+++ components/Phone.vue
@@ -7,11 +7,8 @@
       cover
     >
       <div class="d-flex flex-column h-100">
-        <v-card-title class="d-flex ga-2 px-2">
-        </v-card-title>
-
+        <v-card-title class="d-flex ga-2 px-2"></v-card-title>
         <v-spacer></v-spacer>
-
         <v-card-title class="pb-0 text-center">
           <div class="text-h5">Contacto directo</div>
         </v-card-title>
@@ -19,6 +16,7 @@
     </v-img>
 
     <v-list lines="two">
+      <!-- Número 1 -->
       <v-list-item>
         <template v-slot:prepend>
           <v-avatar>
@@ -27,21 +25,48 @@
         </template>
 
         <v-list-item-title style="font-size: 25px;">+ (502) 5113-8570</v-list-item-title>
-        <v-list-item-subtitle style="color: red; font-size: 15px;">LAW - Firma legal TELEFONO</v-list-item-subtitle>
+        <v-list-item-subtitle style="color: red; font-size: 15px;">
+          LAW - Firma legal TELEFONO
+        </v-list-item-subtitle>
+
+        <template v-slot:append>
+          <v-btn
+            icon
+            variant="text"
+            :href="'https://wa.me/50251138570?text=' + encodeURIComponent('Hola, quisiera más información.')"
+            target="_blank"
+          >
+            <v-icon color="green" icon="mdi-whatsapp"></v-icon>
+          </v-btn>
+        </template>
       </v-list-item>
 
+      <!-- Número 2 -->
       <v-list-item>
         <template v-slot:prepend>
           <v-avatar></v-avatar>
         </template>
 
-        <v-list-item-title style="font-size: 25px;">+ (502) 3338 4962</v-list-item-title>
-        <v-list-item-subtitle style="color: red; font-size: 15px;">LAW - Firma legal TELEFONO</v-list-item-subtitle>
+        <v-list-item-title style="font-size: 25px;">+ (502) 3338 4962</v-list-item-title>
+        <v-list-item-subtitle style="color: red; font-size: 15px;">
+          LAW - Firma legal TELEFONO
+        </v-list-item-subtitle>
 
+        <template v-slot:append>
+          <v-btn
+            icon
+            variant="text"
+            :href="'https://wa.me/50233384962?text=' + encodeURIComponent('Hola, quisiera más información.')"
+            target="_blank"
+          >
+            <v-icon color="green" icon="mdi-whatsapp"></v-icon>
+          </v-btn>
+        </template>
       </v-list-item>
 
       <v-divider inset></v-divider>
 
+      <!-- Email -->
       <v-list-item>
         <template v-slot:prepend>
           <v-avatar>
@@ -50,9 +75,10 @@
         </template>
 
         <v-list-item-title style="font-size: 25px;">lawfirmalegalqt@gmail.com</v-list-item-title>
-        <v-list-item-subtitle style="color: red; font-size: 15px;">LAW - Firma legal CORREO ELECTRONICO</v-list-item-subtitle>
+        <v-list-item-subtitle style="color: red; font-size: 15px;">
+          LAW - Firma legal CORREO ELECTRONICO
+        </v-list-item-subtitle>
       </v-list-item>
-
     </v-list>
   </v-card>
 </template>
